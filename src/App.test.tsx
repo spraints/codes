72b416e4671df3f713c39b73f59964a8618e651a
Matt Burke <spraints@gmail.com>
2021-07-25 14:53:25 -0400

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import React from 'react'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const el = screen.getByText(/Cipher text/i)
+  expect(el).toBeInTheDocument()
 })
